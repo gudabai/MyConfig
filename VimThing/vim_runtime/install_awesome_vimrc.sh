b4be3d703e0fd89f37f45f794ad36e406e2e6e27
fybhp <617243899@qq.com>
2017-03-24 10:16:41 +0800

    remove .

---
+++ VimThing/vim_runtime/install_awesome_vimrc.sh
@@ -0,0 +1,12 @@
+cd ~/.vim_runtime
+
+echo 'set runtimepath+=~/.vim_runtime
+
+source ~/.vim_runtime/vimrcs/basic.vim
+source ~/.vimrc.fyb
+source ~/.vim_runtime/vimrcs/filetypes.vim
+source ~/.vim_runtime/vimrcs/extended.vim
+
+' > ~/.vimrc
+
+echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
