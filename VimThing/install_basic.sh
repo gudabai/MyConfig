558fa8430223f76ce194201d96af2a0e4072f95f
崔舜华 <scui1@xiaohongshu.com>
2018-01-29 17:45:41 +0800

    delete no use thing

--- VimThing/install_basic.sh
+++ VimThing/install_basic.sh
@@ -1,44 +1,12 @@
-VIMRC="~/.vimrc"
-VIM_FOLDER="~/.vim"
-VIMRC_FOLDER="${VIM_FOLDER}/vimrcs"
+mkdir ~/.vim
+cp -r vimrcs/ ~/.vim/vimrcs
 
-if [ -e ${VIMRC} ]
-then
-	echo "${VIMRC} already exists, please delete it first!"
-	exit 1
-else
-	echo "2"
-fi
+echo 'set runtimepath+=~/.vim
 
-# if [ ! -d ${VIM_FOLDER} ]
-# then
-#   	mkdir ${VIM_FOLDER}
-# 	echo "create dir ${VIM_FOLDER}"
-# fi
+source ~/.vim/vimrcs/basic.vim
+source ~/.vim/vimrcs/filetypes.vim
+source ~/.vim/vimrcs/vimrc.fyb
 
-# if [ ! -d ${VIM_FOLDER} ]
-# then
-# 	mkdir ${VIMRC_FOLDER}
-# 	echo "create dir ${VIMRC_FOLDER}"
-# else
-# 	echo "${VIM_FOLDER} already exists, please delete it first!"
-# 	exit 1
-# fi
+' > ~/.vimrc
 
-# if [ "$(ls -A ${VIMRC_FOLDER})" ]
-# then
-# 	echo "your ${VIMRC_FOLDER} is not empty, please empty it first!"
-# 	exit1
-# else
-# 	cp -r ./vimrcs ~/.vim/vimrcs
-# fi
-
-# echo 'set runtimepath+=~/.vim
-
-# source ~/.vim/vimrcs/basic.vim
-# source ~/.vim/vimrcs/filetypes.vim
-# source ~/.vim/vimrcs/vimrc.fyb
-
-# ' > ~/.vimrc
-
-# echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
+echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
